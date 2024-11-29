ad0e631056a1901180b413affebb9902856a7b42
Matheus Melo <matheuspereiramelo7@gmail.com>
2024-11-29 01:25:13 -0300

    initial commit

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,7 +2,7 @@ function CartPage() {
     return (
         <div>
             <h1>Carrinho de Compras</h1>
-            <p>Aqui estarão os produtos adicionados ao carrinho.</p>
+            <p>Aqui estarão os produtos adicionados ao  carrinho.</p>
         </div>
     );
 }
